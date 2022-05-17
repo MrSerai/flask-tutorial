6d84635cfc9f29d5e5c63e2df713d48471d47fe0
MrSerai <allucious7@gmail.com>
2022-05-17 17:03:16 +0200

    Done Until Register a user

---
+++ myproject/flaskr/__init__.py
@@ -0,0 +1,48 @@
+import os 
+from flask import Flask
+
+
+def create_app(test_config=None):
+    #creat and configure the app
+        #create an instance of Flask
+        #__name__ (name of the current py module, its also the path to it)
+        #instance_relative_config=True tells the app that configuration files are relative to the instance folder, which is located outside the flaskr package(should not be commited)
+
+    app=Flask(__name__,instance_relative_config=True) 
+
+    #app.config.from_mapping() sets default configuraation that the app will you
+    app.config.from_mapping(
+        # SECRECT_KEY used by Flask and extensions to keep data safe.set to dev when in development but should be replaced with random  val when deploying 
+        SECRECT_KEY='dev',
+        #DATABASE path to where the SQLlite database file is saved, under the app.instance_path (the path chosen by flask)
+        DATABASE=os.path.join(app.instance_path,'flaskr.sqlite'),
+
+    ) 
+    if test_config is None:
+        #load the instance config, if it exists, when not testing 
+            #app.config.from_pyfile overrides default config with values taken from config.py file  in the instance folder if it exists(eg when deploying it can be used to sset a real SECRET_KEY)  
+        app.config.from_pyfile('config.py',silent=True)
+    else:
+        #load the test config if passed in
+            #test_config can be passed to the factory,will be used instead of the instance config
+        app.config.from_mapping(test_config)
+    #ensure the instance folder
+    try:
+        # os.makedirs ensures that app.instance_path exists(not created automatuically but needs to be ceated, will create the SQLlite file there )
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    #a simple page that says hello
+        # @app.route() creates a simple route so you can see the application working. it creates a connection between URL /hello and a function that returns a response , the string 'Hello, World!'
+    @app.route('/hello')
+    def hello():
+        return 'Hello, world!'
+
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    return app
---
+++ myproject/flaskr/db.py
@@ -0,0 +1,55 @@
+from mimetypes import init
+import sqlite3
+from webbrowser import get
+import click
+import dbus
+from flask import current_app, g
+from flask.cli import with_appcontext
+
+def get_db():
+     #g is a special object and is unique for each request. 
+     # USed to store data can be accessed by multiple functions during the request. 
+     # the connection is stored and reused instead of creating a new connection if get_db is called a second 
+     #      time in the same request. 
+
+    if 'db' not in g:
+            #sqlite3.connect() establish a connection to the file poinnted to at the DATABASE configuration key(doesnt exist yet until initialize the db)
+        g.db=sqlite3.connect(
+            #current_app (special object)Points to the Flask app handling the request
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+                    #sqlite3.Row tells the connection to return rows that behave like dicts, this allows accessing the column by name
+        g.db.row_factory=sqlite3.Row
+    return g.db
+def close_db(e=None):
+    db=g.pop('db',None)
+
+    if db is not None:
+
+        #checks if a connection was created by checking if g.db was set,if the connection exists, it is closed
+        db.close()
+
+"""Add the Python functions that will run these SQL commands to the db.py"""
+def init_db():
+    db=get_db()
+    #open_resource() opens a file relative to the flaskr package. it is necessary because you won't know where that location is when deploying the application 
+    # (get_db returns a database connection, which is used to execute the commands reader from the file) 
+    with current_app.open_resource('schema.sql') as f:
+       # print(f.read().decode('utf8'))
+        db.executescript(f.read().decode('utf8'))
+
+#click.command() defines a command line command called init_db that calls the init_db function and shows a success message to the user
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('intialized the database')
+
+"""Register with the Application"""
+def init_app(app):
+    # app.teardown_appcontext() tells flask to call that function when cleaning up after returning the response 
+    app.teardown_appcontext(close_db)
+    #app.cli.add_command() adds a new command that can be called with the flask command 
+    app.cli.add_command(init_db_command)
